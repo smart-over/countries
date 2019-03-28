00df113c74f72e8a0d3adbef6835b811ae8574de
Ömer Kamçılı <okamcili@smartover.net>
2019-03-28 10:27:13 +0300

    Fix migration

--- src/migrations/2019_03_27_082140_create_country_and_country_translate_tables.php
+++ src/migrations/2019_03_27_082140_create_country_and_country_translate_tables.php
@@ -15,7 +15,7 @@ class CreateCountryAndCountryTranslateTables extends Migration
     {
 
         Schema::create('country', function (Blueprint $table) {
-            $table->integer('id')->unique()->unsigned()->zerofill();
+            $table->integer('id')->unique()->unsigned();
             $table->string('name', 191)->nullable(false);
             $table->string('alpha2Code', 191)->nullable(true);
             $table->string('alpha3Code', 191)->nullable(true);
@@ -23,7 +23,7 @@ class CreateCountryAndCountryTranslateTables extends Migration
             $table->string('subregion', 191)->nullable(true);
             $table->string('timezone', 191)->nullable(true);
             $table->string('nativeName', 191)->nullable(true);
-            $table->integer('numericCode')->unsigned()->nullable(true);
+            $table->string('numericCode', 3)->nullable(true);
             $table->boolean('isDeleted')->nullable(false)->default(0);
 
             $table->index('id', 'id_index');
@@ -31,21 +31,15 @@ class CreateCountryAndCountryTranslateTables extends Migration
             $table->index(['id', 'name'], 'id_name_index');
         });
 
-        \Illuminate\Support\Facades\DB::statement('ALTER TABLE country CHANGE id id INT(3) UNSIGNED ZEROFILL NOT NULL');
-        \Illuminate\Support\Facades\DB::statement('ALTER TABLE country CHANGE numericCode numericCode INT(3) UNSIGNED ZEROFILL NOT NULL');
-
         Schema::create('countryTranslate', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->integer('countryId')->unsigned()->zerofill();
+            $table->integer('countryId')->unsigned();
             $table->string('langCode');
             $table->string('translateName')->nullable(true);
 
             $table->index(['countryId', 'langCode'], 'country_id_and_lang_code_index');
             $table->foreign('countryId', 'country_id_foreign')->references('id')->on('country')->onDelete('cascade');
-
         });
-
-        \Illuminate\Support\Facades\DB::statement('ALTER TABLE countryTranslate CHANGE countryId countryId INT(3) UNSIGNED ZEROFILL NOT NULL');
     }
 
     /**
